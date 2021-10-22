77120fdaadc6570c94e55e11041e2f48403801f5
ajaye2 <ajaye2@illinois.edu>
2021-10-22 14:36:16 -0500

    "init"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,36 +4,49 @@ import About from './About/About';
 import Projects from './Projects/Projects';
 import Contact from './Contact/Contact';
 import Footer from './Footer/Footer';
-import Temp from './Temp/Temp';
+import Education from './Education/Education';
+import WorkExperience from './WorkExperience/WorkExperience';
+import Certificates from './Certificates/Certificates';
+import Blogs from './Blogs/Blogs';
+
 
 import { PortfolioProvider } from '../context/context';
 
-import { heroData, aboutData, projectsData, tempData, contactData, footerData } from '../mock/data';
+import { heroData, aboutData, projectsData, educationData, certificatesData, workExperienceData, contactData, footerData, blogsData } from '../mock/data';
 
 function App() {
   const [hero, setHero] = useState({});
   const [about, setAbout] = useState({});
+  const [education, setEducation] = useState([]);
+  const [workExperience, setworkExperience] = useState([]);
   const [projects, setProjects] = useState([]);
-  const [temp, setTemp] = useState([]);
+  const [blogs, setBlogs] = useState([]);
+  const [certificates, setCertificates] = useState([]);
   const [contact, setContact] = useState({});
   const [footer, setFooter] = useState({});
 
   useEffect(() => {
     setHero({ ...heroData });
     setAbout({ ...aboutData });
+    setEducation([...educationData]);
+    setworkExperience([...workExperienceData]);
     setProjects([...projectsData]);
-    setTemp({...tempData});
+    setBlogs([...blogsData]);
+    setCertificates([...certificatesData]);
     setContact({ ...contactData });
     setFooter({ ...footerData });
   }, []);
 
   return (
-    <PortfolioProvider value={{ hero, about, projects, temp, contact, footer }}>
+    <PortfolioProvider value={{ hero, about, projects, education, certificates, workExperience, blogs, contact, footer }}>
       <Hero />
       <About />
-      <Projects />
-      <Temp />
-      <Projects />
+      <Education />     {/* reduce image size */}
+      <WorkExperience /> {/* reduce image size */}
+      {/* Publications */} {/* Add details  */}
+      <Projects />  {/* Add details  */}
+      <Certificates /> {/* Certificates */}
+      <Blogs />
       <Contact />
       <Footer />
     </PortfolioProvider>
